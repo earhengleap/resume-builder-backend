23e1cacfd9079235d5e8c5b93fc07c4b4b935109
unknown <hengleap70@gmail.com>
2025-06-10 09:05:20 +0700

    initial commit

--- controllers/resume-controller.js
+++ controllers/resume-controller.js
@@ -168,7 +168,59 @@ const updateResume = async (req, res) => {
 // @route   DELETE /api/resume/:id
 // @access  Private
 
-const deleteResume = async (req, res) => {};
+const deleteResume = async (req, res) => {
+  try {
+    const resume = await Resume.findOne({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
+
+    if (!resume) {
+      return res
+        .status(404)
+        .json({ message: "Resume not found or unauthorized" });
+    }
+
+    // Delete thumbnailLink and profilePreviewUrl images from uploads folder
+    // const uploadsFolder = path.join(__dirname, "..", "uploads");
+    const uploadsFolder = path.join(process.cwd(), "uploads");
+
+    const baseUrl = `${req.protocol}://${req.get("host")}`;
+
+    if (resume.thumbnailLink) {
+      const oldThumbnail = path.join(
+        uploadsFolder,
+        path.basename(resume.thumbnailLink)
+      );
+      if (fs.existsSync(oldThumbnail)) fs.unlinkSync(oldThumbnail);
+    }
+
+    if (resume.profileInfo?.profilePreviewUrl) {
+      const oldProfile = path.join(
+        uploadsFolder,
+        path.basename(resume.profileInfo.profilePreviewUrl)
+      );
+      if (fs.existsSync(oldProfile)) fs.unlinkSync(oldProfile);
+    }
+
+    const deleted = await Resume.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
+
+    if (!deleted) {
+      return res
+        .status(404)
+        .json({ message: "Resume not found or unauthorized" });
+    }
+
+    res.json({ message: "Resume deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to delete resume", error: error.message });
+  }
+};
 
 export {
   createResume,
